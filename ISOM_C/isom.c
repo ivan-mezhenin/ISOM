2165c89da3174f9b41d09dd356eba24bc5654de9
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-06-08 08:12:34 +0300

    feat: check isomorphism

--- ISOM_C/isom.c
+++ ISOM_C/isom.c
@@ -165,3 +165,238 @@ int get_variables(Hypergraph* g, char res[][MAX_NAME]) {
     return cnt;
 }
 
+// Build template H from G1, returns λ1: const_G1 -> x_i
+// Variables in G1 are kept as-is (they become part of H)
+Hypergraph* build_template(Hypergraph* g1, Unifier* lambda1) {
+    Hypergraph* H = (Hypergraph*)calloc(1, sizeof(Hypergraph));
+    H->edge_cnt = g1->edge_cnt;
+    H->arity = g1->arity;
+    lambda1->cnt = 0;
+    
+    // Collect constants and assign them new variable names
+    char consts[MAX_ARGS][MAX_NAME] = {{0}};
+    int ccnt = 0;
+    
+    for (int i = 0; i < g1->edge_cnt; i++) {
+        for (int j = 0; j < g1->edges[i].arity; j++) {
+            char* arg = g1->edges[i].args[j];
+            if (!is_var(arg)) {  // It's a constant
+                bool found = false;
+                for (int k = 0; k < ccnt; k++) {
+                    if (strcmp(consts[k], arg) == 0) { found = true; break; }
+                }
+                if (!found && ccnt < MAX_ARGS) {
+                    copy_str(consts[ccnt], arg);
+                    // Create mapping: constant -> new variable x_i
+                    char vn[MAX_NAME];
+                    sprintf(vn, "x%d", ccnt + 1);
+                    copy_str(lambda1->subs[lambda1->cnt].var, consts[ccnt]);
+                    copy_str(lambda1->subs[lambda1->cnt].val, vn);
+                    lambda1->cnt++;
+                    ccnt++;
+                }
+            }
+        }
+    }
+    
+    // Build H: replace constants with their x_i, keep variables as-is
+    for (int i = 0; i < g1->edge_cnt; i++) {
+        copy_str(H->edges[i].pred, g1->edges[i].pred);
+        H->edges[i].arity = g1->edges[i].arity;
+        for (int j = 0; j < g1->edges[i].arity; j++) {
+            char* arg = g1->edges[i].args[j];
+            if (is_var(arg)) {
+                // Variable in G1 stays as-is in H
+                copy_str(H->edges[i].args[j], arg);
+            } else {
+                // Constant: replace with its variable
+                for (int k = 0; k < lambda1->cnt; k++) {
+                    if (strcmp(arg, lambda1->subs[k].var) == 0) {
+                        copy_str(H->edges[i].args[j], lambda1->subs[k].val);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return H;
+}
+
+Hypergraph* apply_uni(Hypergraph* g, Unifier* u) {
+    Hypergraph* res = (Hypergraph*)calloc(1, sizeof(Hypergraph));
+    res->edge_cnt = g->edge_cnt;
+    res->arity = g->arity;
+    
+    for (int i = 0; i < g->edge_cnt; i++) {
+        copy_str(res->edges[i].pred, g->edges[i].pred);
+        res->edges[i].arity = g->edges[i].arity;
+        for (int j = 0; j < g->edges[i].arity; j++) {
+            bool found = false;
+            for (int k = 0; k < u->cnt; k++)
+                if (strcmp(g->edges[i].args[j], u->subs[k].var) == 0) {
+                    copy_str(res->edges[i].args[j], u->subs[k].val);
+                    found = true;
+                    break;
+                }
+            if (!found) copy_str(res->edges[i].args[j], g->edges[i].args[j]);
+        }
+    }
+    return res;
+}
+
+// Compose u1 and u2: apply u2 to values of u1
+Unifier* compose(Unifier* u1, Unifier* u2) {
+    Unifier* res = (Unifier*)calloc(1, sizeof(Unifier));
+    for (int i = 0; i < u1->cnt && res->cnt < MAX_ARGS * 2; i++) {
+        copy_str(res->subs[res->cnt].var, u1->subs[i].var);
+        char fv[MAX_NAME] = {0};
+        copy_str(fv, u1->subs[i].val);
+        for (int j = 0; j < u2->cnt; j++)
+            if (strcmp(fv, u2->subs[j].var) == 0) {
+                copy_str(fv, u2->subs[j].val);
+                break;
+            }
+        copy_str(res->subs[res->cnt].val, fv);
+        res->cnt++;
+    }
+    return res;
+}
+
+// --- Isomorphism checks ---
+bool is_literal_iso(char a1[][MAX_NAME], char a2[][MAX_NAME], int arity) {
+    int i1 = 0, i2 = 0;
+    
+    for (int i = 0; i < arity; i++) {
+        int id1 = -1;
+        for (int j = 0; j < i1; j++) if (strcmp(a1[i], a1[j]) == 0) { id1 = j; break; }
+        if (id1 == -1) { id1 = i1; i1++; }
+        
+        int id2 = -1;
+        for (int j = 0; j < i2; j++) if (strcmp(a2[i], a2[j]) == 0) { id2 = j; break; }
+        if (id2 == -1) { id2 = i2; i2++; }
+        
+        if (id1 != id2) return false;
+    }
+    return true;
+}
+
+bool edge_is_iso(Hyperedge* e1, Hyperedge* e2) {
+    if (e1->arity != e2->arity) return false;
+    if (strcmp(e1->pred, e2->pred) != 0) return false;
+    return is_literal_iso(e1->args, e2->args, e1->arity);
+}
+
+bool hg_is_iso(Hypergraph* g1, Hypergraph* g2) {
+    if (g1->edge_cnt != g2->edge_cnt) return false;
+    bool used[MAX_EDGES] = {false};
+    for (int i = 0; i < g1->edge_cnt; i++) {
+        bool found = false;
+        for (int j = 0; j < g2->edge_cnt; j++) {
+            if (!used[j] && edge_is_iso(&g1->edges[i], &g2->edges[j])) {
+                used[j] = true;
+                found = true;
+                break;
+            }
+        }
+        if (!found) return false;
+    }
+    return true;
+}
+
+// --- Find λ2 (stop at first) ---
+bool search_lambda2(Hypergraph* H, Hypergraph* G2,
+                    Unifier* cur, int depth,
+                    char vars[][MAX_NAME], int var_cnt,
+                    char consts[][MAX_NAME], int const_cnt,
+                    bool used_consts[]) {
+    if (depth == var_cnt) {
+        Hypergraph* Hp = apply_uni(H, cur);
+        if (Hp && hg_is_iso(Hp, G2)) {
+            // Found! Copy result to cur (it's already there)
+            if (Hp) free_hg(Hp);
+            return true;
+        }
+        if (Hp) free_hg(Hp);
+        return false;
+    }
+    
+    char var[MAX_NAME];
+    copy_str(var, vars[depth]);
+    
+    for (int i = 0; i < const_cnt; i++) {
+        if (used_consts[i]) continue;
+        
+        copy_str(cur->subs[cur->cnt].var, var);
+        copy_str(cur->subs[cur->cnt].val, consts[i]);
+        cur->cnt++;
+        used_consts[i] = true;
+        
+        if (search_lambda2(H, G2, cur, depth + 1, vars, var_cnt, consts, const_cnt, used_consts)) {
+            return true;  // Stop at first found
+        }
+        
+        cur->cnt--;
+        used_consts[i] = false;
+    }
+    return false;
+}
+
+// Get ALL unique arguments (variables and constants) from a hypergraph
+int get_all_args(Hypergraph* g, char res[][MAX_NAME]) {
+    int cnt = 0;
+    for (int i = 0; i < g->edge_cnt; i++)
+        for (int j = 0; j < g->edges[i].arity; j++) {
+            bool found = false;
+            for (int k = 0; k < cnt; k++)
+                if (strcmp(res[k], g->edges[i].args[j]) == 0) { found = true; break; }
+            if (!found && cnt < MAX_ARGS) copy_str(res[cnt++], g->edges[i].args[j]);
+        }
+    return cnt;
+}
+
+// --- Main function: returns ONE unifier or NULL ---
+Unifier* check_iso(Hypergraph* g1, Hypergraph* g2) {
+    // 1. Check characteristics (necessary condition from PDF)
+    GraphChar* c1 = calc_char(g1);
+    GraphChar* c2 = calc_char(g2);
+    
+    if (!cmp_chars(c1, c2)) {
+        free_char(c1); free_char(c2);
+        return NULL;
+    }
+    
+    // 2. Build template H and λ1: constants of G1 -> variables x_i
+    Unifier lambda1;
+    lambda1.cnt = 0;
+    Hypergraph* H = build_template(g1, &lambda1);
+    
+    // 3. Get variables in H and ALL arguments in G2 (both vars and constants)
+    char vars[MAX_ARGS][MAX_NAME] = {{0}};
+    int var_cnt = get_variables(H, vars);
+    
+    char all_args[MAX_ARGS][MAX_NAME] = {{0}};
+    int all_cnt = get_all_args(g2, all_args);
+    
+    // 4. Search for λ2 (stop at first)
+    Unifier lambda2;
+    lambda2.cnt = 0;
+    bool used_args[MAX_ARGS] = {false};
+    
+    if (!search_lambda2(H, g2, &lambda2, 0, vars, var_cnt, all_args, all_cnt, used_args)) {
+        // No isomorphism found
+        free_hg(H);
+        free_char(c1);
+        free_char(c2);
+        return NULL;
+    }
+    
+    // 5. Compose λ = λ1 ∘ λ2
+    Unifier* result = compose(&lambda1, &lambda2);
+    
+    // Cleanup
+    free_hg(H);
+    free_char(c1);
+    free_char(c2);
+    
+    return result;
+}
