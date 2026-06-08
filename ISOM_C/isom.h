f0819684885dcd59e4b06327a000d25ca1b7ba6b
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-06-08 07:54:50 +0300

    feat: add header file with data structures and declarations

---
+++ ISOM_C/isom.h
@@ -0,0 +1,43 @@
+#ifndef ISOM_H
+#define ISOM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_NAME 50
+#define MAX_ARGS 20
+#define MAX_EDGES 100
+
+typedef struct {
+    char pred[MAX_NAME];
+    char args[MAX_ARGS][MAX_NAME];
+    int arity;
+} Hyperedge;
+
+typedef struct {
+    Hyperedge edges[MAX_EDGES];
+    int edge_cnt;
+    int arity;
+} Hypergraph;
+
+typedef struct {
+    char var[MAX_NAME];
+    char val[MAX_NAME];
+} Sub;
+
+typedef struct {
+    Sub subs[MAX_ARGS * 2];
+    int cnt;
+} Unifier;
+
+// Функции
+Hypergraph* parse_hg(const char* str);
+void free_hg(Hypergraph* g);
+void print_hg(Hypergraph* g);
+
+// Главная функция: возвращает ОДИН унификатор (или NULL, если не изоморфны)
+Unifier* check_iso(Hypergraph* g1, Hypergraph* g2);
+
+#endif
